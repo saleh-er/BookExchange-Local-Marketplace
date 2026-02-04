ea0dde95799896ce8eb919829ccfc25c5089cb9e
saleh <saleherragragui02@gmail.com>
2026-02-04 16:19:31 +0100

    feat: register Book model views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book
+from .forms import BookForm
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all().order_by('-created_at')
+    return render(request, 'books/book_list.html', {'books': books})
+
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.seller = request.user # Links the book to the logged-in user
+            book.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form': form})
