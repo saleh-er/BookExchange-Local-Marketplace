5e6ffc37de48fb7584d9b5b63b8e05874dfd2557
saleh <saleherragragui02@gmail.com>
2026-02-04 16:16:45 +0100

    feat: register Book model in admin panel with filters

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price', 'condition', 'seller')
+    search_fields = ('title', 'author')
+    list_filter = ('condition',)
