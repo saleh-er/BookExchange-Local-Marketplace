5612ef3b831421921e86734548ebba16b99bf4d7
saleh <saleherragragui02@gmail.com>
2026-02-04 16:15:34 +0100

    feat: add Book model with title, price, and condition

--- books/models.py
+++ books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Book(models.Model):
+    CONDITION_CHOICES = [
+        ('NEW', 'New'),
+        ('GOOD', 'Good'),
+        ('FAIR', 'Fair/Used'),
+    ]
+
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    condition = models.CharField(max_length=10, choices=CONDITION_CHOICES, default='GOOD')
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='book_covers/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
