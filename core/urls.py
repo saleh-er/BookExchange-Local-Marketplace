eb9a4c897d726d931d66bc99f539cbdf9fbd0338
saleh <saleherragragui02@gmail.com>
2026-02-04 16:30:55 +0100

    fix: remove typo in admin URL configuration

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.py.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('books.urls')), # Connects our app to the root URL
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
